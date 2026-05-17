526876532397cac88ada6a6b4d1097e31c320116
Krish-S25 <krishangs255@gmail.com>
2026-05-17 20:37:07 +0530

    Add: valid-palindrome-ii - submission-4

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-4.cpp
@@ -0,0 +1,32 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+private:
+    bool isPalindrome(const string& s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+public:
+    bool validPalindrome(string s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
