a47b1d5d682f1b73c4c6c12e3e91228809b0482b
Krish-S25 <krishangs255@gmail.com>
2026-05-26 18:47:03 +0530

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <string>
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        int cs = 0;
+        vector<int> st ; 
+        for(const string& k : operations){
+            if (k =="+"){
+                int t1 , t2 , sm ;
+                t2 = st.back();
+                st.pop_back();
+                t1 = st.back();
+                sm = t1+t2;
+                st.push_back(t2);
+                st.push_back(sm);
+            }
+            else if (k == "D"){
+                int t1 , sm;
+                t1 = st.back();
+                st.push_back(2*t1);
+            }
+            else if (k == "C"){
+                st.pop_back();
+            }
+            else{
+                st.push_back(stoi(k));
+            }
+        }
+        for (auto i : st) {
+            cs += i;
+        }
+
+        return cs;
+    }
+};
