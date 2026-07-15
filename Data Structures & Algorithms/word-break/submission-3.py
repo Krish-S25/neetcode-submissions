4835be447bed68ffc4ba37ec88b7fd0aff2a2d29
Krish-S25 <krishangs255@gmail.com>
2026-07-16 00:25:26 +0530

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+    
+        n = len(s)                #OPTIMAL Bottoms up DP
+        dp = [False] * (n+1)      #Time complexity O(n^2 . t)
+        dp[0] = True
+
+        word_set = set(wordDict)
+        
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+            
+
+        return dp[n]
+
+        
