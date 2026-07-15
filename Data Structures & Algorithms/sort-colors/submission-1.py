e0da87eb1a36e0d67d3d5872bbad686e79397303
Krish-S25 <krishangs255@gmail.com>
2026-07-16 02:04:10 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        #Binary search DUTCH flag
+
+        low = 0
+        high = len(nums)-1 
+        mid = 0  
+
+        while(mid <= high):
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            
+            elif nums[mid] == 1:
+                mid += 1
+            
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+        
